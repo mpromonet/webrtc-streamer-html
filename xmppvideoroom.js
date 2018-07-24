5deb161c171e65b5451d5db3c4d4662432b60cc7
Michel Promonet <michel.promonet@free.fr>
2018-07-24 23:07:22 +0200

    use one connection per join

--- xmppvideoroom.js
+++ xmppvideoroom.js
@@ -12,12 +12,6 @@ var XMPPVideoRoom = (function() {
 		this.handlers    = [];
 		this.srvurl      = srvurl || location.protocol+"//"+window.location.hostname+":"+window.location.port;
 		this.sessionList = {};
-		this.onConnectCallback = [];
-
-		this.connection = new Strophe.Connection(location.protocol+ "//" + xmppUrl + "/http-bind");
-		this.connection.addHandler(this.OnJingle, 'urn:xmpp:jingle:1', 'iq', 'set', null, null);
-		var bind = this;
-		this.connection.connect(xmppUrl, null, function(status) { bind.onConnect(status); });
 	};
 		
 
@@ -28,28 +22,18 @@ var XMPPVideoRoom = (function() {
 	* @param {string} name - name in Video Room
 	*/
 	XMPPVideoRoom.prototype.join = function(roomid, url, name) {
-
-		var roomUrl = roomid + "@" + "conference." + this.xmppUrl;
-		var extPresence = Strophe.xmlElement('nick', {xmlns:'http://jabber.org/protocol/nick'}, name);
-
 		var bind = this;
-		if (this.status === Strophe.Status.CONNECTING) {
-			this.onConnectCallback.push(function() { bind.connection.muc.join(roomUrl, name, bind.OnMessage, bind.OnPresence, null, null, null, extPresence); } );	
-		} else {
-			bind.connection.muc.join(roomUrl, name, bind.OnMessage, bind.OnPresence, null, null, null, extPresence);		
-		}
-	}
-		
-	XMPPVideoRoom.prototype.OnMessage = function(data)  {
-		console.log("OnMessage from:" + data.getAttribute("from") + " to:" + data.getAttribute("to") + " msg:" + data.textContent) 
-		return true;
-	}
-	XMPPVideoRoom.prototype.OnPresence = function(data) {
-		console.log("OnPresence from:" + data.getAttribute("from") + " to:" + data.getAttribute("to") );
-		return true;
+
+		var connection = new Strophe.Connection(location.protocol+ "//" + this.xmppUrl + "/http-bind");
+		connection.addHandler(function(iq) { return this.OnJingle(connection, iq, url) }, 'urn:xmpp:jingle:1', 'iq', 'set', null, null);
+
+//		connection.rawInput = function (data) { console.log('RECV: ' + data); };
+//		connection.rawOutput = function (data) { console.log('SEND: ' + data); };
+
+		connection.connect(this.xmppUrl, null, function(status) { bind.onConnect(connection, roomid, name, status); });
 	}
 
-	XMPPVideoRoom.prototype.onReceiveCandidate = function(iq, candidateList) {
+	XMPPVideoRoom.prototype.onReceiveCandidate = function(connection, iq, candidateList) {
 		console.log("============candidateList:" +  JSON.stringify(candidateList));
 		var jingle = iq.querySelector("jingle");
 		var sid = jingle.getAttribute("sid");
@@ -63,7 +47,7 @@ var XMPPVideoRoom = (function() {
 			var jingle = param.c('jingle', {xmlns: 'urn:xmpp:jingle:1'});
 			jingle.attrs({ action: "transport-info",  sid });
 
-			var id = this.connection.sendIQ(jingle, () => {
+			var id = connection.sendIQ(jingle, () => {
 				console.log("============transport-info ok sid:" + sid);		
 			},() => {
 				console.log("############transport-info error sid:" + sid);
@@ -71,7 +55,7 @@ var XMPPVideoRoom = (function() {
 		});	
 	}
 
-	XMPPVideoRoom.prototype.onCall = function(iq, data) {
+	XMPPVideoRoom.prototype.onCall = function(connection, iq, data) {
 		console.log("webrtc answer========================" + data.sdp);		
 		
 		var jingle = iq.querySelector("jingle");
@@ -83,13 +67,13 @@ var XMPPVideoRoom = (function() {
 		jingle.attrs({ action: "session-accept",  sid, responder:iq.getAttribute("to") });
 
 		var jingleanswer = sdp.toJingle(jingle); 
-		var id = this.connection.sendIQ(jingleanswer, () => {
+		var id = connection.sendIQ(jingleanswer, () => {
 			console.log("============session-accept ok sid:" + sid);
 				
 			var method = this.srvurl + "/api/getIceCandidate?peerid="+ sid;
 			request("GET" , method).done( function (response) { 
 					if (response.statusCode === 200) {
-						this.onReceiveCandidate(jingleanswer.node,JSON.parse(response.body));
+						this.onReceiveCandidate(connection, jingleanswer.node, JSON.parse(response.body));
 					}
 					else {
 						this.onError(response.statusCode);
@@ -105,7 +89,7 @@ var XMPPVideoRoom = (function() {
 		console.log("############onError:" + error)
 	}
 		
-	XMPPVideoRoom.prototype.OnJingle = function(iq) {
+	XMPPVideoRoom.prototype.OnJingle = function(connection, iq, url) {
 		console.log("OnJingle from:" + iq.getAttribute("from") + " to:" + iq.getAttribute("to") + " action:" +  iq.querySelector("jingle").getAttribute("action"));
 		var jingle = iq.querySelector("jingle");
 		var sid = jingle.getAttribute("sid");
@@ -116,25 +100,23 @@ var XMPPVideoRoom = (function() {
 			sdp.fromJingle($(jingle));
 
 			console.log("xmpp offer============sdp:" + sdp.raw);
-			var webrtcStream = document.querySelector('#webrtcStream').value;
-			var method = this.srvurl + "/api/call?peerid="+ sid +"&url="+encodeURIComponent(webrtcStream)+"&options="+encodeURIComponent("rtptransport=tcp");
+			var method = this.srvurl + "/api/call?peerid="+ sid +"&url="+encodeURIComponent(url)+"&options="+encodeURIComponent("rtptransport=tcp&timeout=60");
 			request("POST" , method, {body:JSON.stringify({type:"offer",sdp:sdp.raw})}).done( function (response) { 
 					if (response.statusCode === 200) {
-						this.onCall(iq,JSON.parse(response.body));
+						this.onCall(connection, iq, JSON.parse(response.body));
 					}
 					else {
 						this.onError(response.statusCode);
 					}
 				}
 			);
-			this.sessionList[sid]=iq;
+			this.sessionList[sid]=connection;
 			
 			var ack = $iq({ type: "result",  from: iq.getAttribute("to"), to: iq.getAttribute("from"), id:iq.getAttribute("id") })
-			this.connection.sendIQ(ack);		
+			connection.sendIQ(ack);		
 
 		} else if (action === "transport-info") {
 
-			var session = this.sessionList[sid];
 			console.log("xmpp candidate============sid:" + sid);
 
 			var contents = $(jingle).find('>content');
@@ -165,15 +147,14 @@ var XMPPVideoRoom = (function() {
 			});
 	
 			var ack = $iq({ type: "result",  from: iq.getAttribute("to"), to: iq.getAttribute("from"), id:iq.getAttribute("id") })
-			this.connection.sendIQ(ack);		
+			connection.sendIQ(ack);		
 		}
 					
 		return true;		
 	}
 	
-	XMPPVideoRoom.prototype.onConnect = function(status)
+	XMPPVideoRoom.prototype.onConnect = function(connection, roomid, name, status)
 	{		
-		this.status = status;
 	    if (status === Strophe.Status.CONNECTING) {
 			console.log('Strophe is connecting.');
 	    } else if (status === Strophe.Status.CONNFAIL) {
@@ -186,53 +167,49 @@ var XMPPVideoRoom = (function() {
 			console.log('Strophe is connected.');
 			
 			// disco stuff
-			if (this.connection.disco) {
-				this.connection.disco.addIdentity('client', 'web');
-				this.connection.disco.addFeature(Strophe.NS.DISCO_INFO);
-				this.connection.disco.addFeature(Strophe.NS.CAPS);
-				this.connection.disco.addFeature("urn:xmpp:jingle:1");
-				this.connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:1");
-				this.connection.disco.addFeature("urn:xmpp:jingle:transports:ice-udp:1");
-				this.connection.disco.addFeature("urn:xmpp:jingle:transports:raw-udp:1");
-				this.connection.disco.addFeature("urn:xmpp:jingle:apps:dtls:0");
-				this.connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:audio");
-				this.connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:video");
-				this.connection.disco.addFeature("urn:ietf:rfc:5761") // rtcp-mux
+			if (connection.disco) {
+				connection.disco.addIdentity('client', 'web');
+				connection.disco.addFeature(Strophe.NS.DISCO_INFO);
+				connection.disco.addFeature("urn:xmpp:jingle:1");
+				connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:1");
+				connection.disco.addFeature("urn:xmpp:jingle:transports:ice-udp:1");
+				connection.disco.addFeature("urn:xmpp:jingle:transports:raw-udp:1");
+				connection.disco.addFeature("urn:xmpp:jingle:apps:dtls:0");
+				connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:audio");
+				connection.disco.addFeature("urn:xmpp:jingle:apps:rtp:video");
+				connection.disco.addFeature("urn:ietf:rfc:5761") // rtcp-mux
 			}
 
-			while (this.onConnectCallback.length) {
-				var cb = this.onConnectCallback.shift();
-				cb();
-			}
+			var roomUrl = roomid + "@" + "conference." + this.xmppUrl;
+			var extPresence = Strophe.xmlElement('nick', {xmlns:'http://jabber.org/protocol/nick'}, name);
+			connection.muc.join(roomUrl, name, null, null, null, null, null, extPresence);		
 		}
-		
 	}
 		
-	XMPPVideoRoom.prototype.disconnect = function () {
-		if (this.connection) {
-			Object.keys(this.sessionList).forEach( (sid) => {
-			
-				var param = $iq({ type: "set",  from: roomUrl +"/" + userName, to: roomUrl })
-				var jingle = param.c('jingle', {xmlns: 'urn:xmpp:jingle:1'});
-				jingle.attrs({ action: "session-terminate",  sid});
-			
-				var method = this.srvurl + "/api/hangup?peerid="+ sid;
-				request("GET" , method).done( function (response) { 
-						if (response.statusCode === 200) {
-							console.log("method:"+method+ " answer:" +response.body);
-						}
-						else {
-							this.onError(response.statusCode);
-						}
+	XMPPVideoRoom.prototype.leave = function (roomid, userName) {
+		Object.entries(this.sessionList).forEach( (sid,connection) => {
+			var roomUrl = roomid + "@" + "conference." + this.xmppUrl;
+
+			var param = $iq({ type: "set",  from: roomUrl +"/" + userName, to: roomUrl })
+			var jingle = param.c('jingle', {xmlns: 'urn:xmpp:jingle:1'});
+			jingle.attrs({ action: "session-terminate",  sid});
+			connection.sendIQ(param);
+
+			var method = this.srvurl + "/api/hangup?peerid="+ sid;
+			request("GET" , method).done( function (response) { 
+					if (response.statusCode === 200) {
+						console.log("method:"+method+ " answer:" +response.body);
 					}
-				);					
-			});
-		
-			this.connection.muc.leave(roomUrl, userName);
-			this.connection.flush();
-			this.connection.disconnect();
-		}
-		this.connection = null;
+					else {
+						this.onError(response.statusCode);
+					}
+				}
+			);					
+			connection.muc.leave(roomUrl, userName);
+			connection.flush();
+			connection.disconnect();	
+		});
+		this.sessionList = {};
 	}
 
 	return XMPPVideoRoom;
