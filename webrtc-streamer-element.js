0c47f06ad67104716057bfa4c596c9833b8894b1
Michel Promonet <michel.promonet.1@gmail.com>
2019-05-12 22:08:38 +0000

    extract webcomponent in javascript

---
+++ webrtc-streamer-element.js
@@ -0,0 +1,51 @@
+import "./libs/request.min.js";
+
+class WebRTCStreamerElement extends HTMLElement {
+	static get observedAttributes() {
+		return ['videostream','audiostream','options', 'webrtcurl'];
+	}  
+	
+	constructor() {
+		super(); 
+		this.shadowDOM = this.attachShadow({mode: 'open'});
+		this.shadowDOM.innerHTML = `
+					<style>@import "styles.css"</style>
+					<h2 id="title"></h2>
+					<video id="video"></video>
+					`;
+	}
+	connectedCallback() {
+		this.connectStream();
+	}
+	disconnectedCallback() {
+		this.disconnectStream();
+	}
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		this.connectStream();
+	}
+	
+	disconnectStream() {
+		if (this.webRtcServer) {
+			this.webRtcServer.disconnect();
+			this.webRtcServer = null;
+		}
+	}
+	connectStream() {
+		this.disconnectStream();
+		
+		let webrtcurl = this.getAttribute("webrtcurl") || webrtcConfig.url;
+		let videostream = this.getAttribute("videostream") || webrtcConfig.defaultvideostream;
+		let audiostream = this.getAttribute("audiostream") || webrtcConfig.defaultaudiostream;
+		let options = this.getAttribute("options") || webrtcConfig.options;
+		
+		this.shadowDOM.getElementById("title").innerHTML = videostream; 
+		
+		let videoElement = this.shadowDOM.getElementById("video");
+		this.webRtcServer = new WebRtcStreamer(videoElement, webrtcurl);
+		this.webRtcServer.connect(videostream, audiostream, options);
+	}
+	
+	
+}
+
+customElements.define('webrtc-streamer', WebRTCStreamerElement);
