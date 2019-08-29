2b435d3fbe0f6b67b067f09b5617d7bc43ba44a0
Michel Promonet <michel.promonet@free.fr>
2019-08-29 19:24:34 +0200

    add webcomponent to select a stream

---
+++ webrtc-streamer-selector-element.js
@@ -0,0 +1,53 @@
+import "./libs/request.min.js";
+
+class WebRTCStreamerSelectorElement extends HTMLElement {	
+	static get observedAttributes() {
+		return ['selected'];
+	}  	
+	constructor() {
+		super(); 
+		this.shadowDOM = this.attachShadow({mode: 'open'});
+		this.shadowDOM.innerHTML = `
+					<style>@import "styles.css"</style>
+					<h2><select id="mediaList"></select></h2>
+					`;
+	}
+	connectedCallback() {
+		let mediaList = this.shadowDOM.getElementById("mediaList");
+		request("GET" , webrtcConfig.url + "/api/getMediaList").done( (response) => { 
+			JSON.parse(response.body).forEach( (media) => {
+				var newOption = document.createElement("option");
+				newOption.text = media.video;
+				newOption.value = JSON.stringify(media);
+				if (this.selected && (this.selected === newOption.text) ) {
+					newOption.selected = true;
+				}
+				mediaList.appendChild(newOption);
+			});
+
+			if (mediaList.selectedOptions.length > 0) {
+				this.dispatchEvent(new CustomEvent('change', {
+					detail: {
+					  url: JSON.parse(mediaList.selectedOptions[0].value),
+					}
+				  }));				
+			}	
+		});	
+
+		mediaList.onchange = (event) => {
+				this.dispatchEvent(new CustomEvent('change', {
+					detail: {
+					  url: JSON.parse(mediaList.selectedOptions[0].value),
+					}
+				  }));		
+			}
+
+	}
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		if (attrName === "selected") {
+			this.selected = newVal;
+		}
+	}	
+}
+
+customElements.define('webrtc-streamer-selector', WebRTCStreamerSelectorElement);
