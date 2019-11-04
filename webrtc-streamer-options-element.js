d6c2b20565300712935d1f7d9ae3e13c1df97f56
Michel Promonet <michel.promonet@free.fr>
2019-11-04 11:30:33 +0100

    make some tries to add a options panel

---
+++ webrtc-streamer-options-element.js
@@ -0,0 +1,65 @@
+import "./libs/request.min.js";
+
+class WebRTCStreamerOptionsElement extends HTMLElement {	
+	constructor() {
+		super(); 
+		this.shadowDOM = this.attachShadow({mode: 'open'});
+		this.shadowDOM.innerHTML = `
+					<style>@import "styles.css"</style>
+					<h2>Options</h2>
+					<div id="options"></div>
+					<button id="apply">Apply</button>
+					`;
+		this.button = this.shadowDOM.getElementById("apply");
+		this.button.onclick = () => this.notifyOptions();
+		this.params = new URLSearchParams();
+	}
+
+	static get observedAttributes() {
+		return ['options'];
+	}  
+
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		if (attrName === "options") {
+			this.fillOptions();
+		}
+	}
+
+	connectedCallback() {
+		this.fillOptions();
+	}
+
+	fillOptions() {
+		const options = this.getAttribute("options") || "";
+		this.params = new URLSearchParams(options);
+
+		let optElement = this.shadowDOM.getElementById("options");
+		optElement.innerHTML = "";
+
+		for (let [k,v] of this.params.entries()) {
+			let label = document.createTextNode(k+":");
+			optElement.appendChild(label);			
+			let input = document.createElement("input");
+			input.type = "text";
+			input.value = v;
+			input.onchange = () => {
+				this.params.set(k, input.value);
+
+			}
+			optElement.appendChild(input);		
+			let br = document.createElement("br");
+			optElement.appendChild(br);		
+		}	
+	}
+
+	notifyOptions()
+	{
+		this.dispatchEvent(new CustomEvent('change', {
+		detail: {
+		  options: this.params.toString(),
+		}
+	  }));
+	}
+}
+
+customElements.define('webrtc-streamer-options', WebRTCStreamerOptionsElement);
