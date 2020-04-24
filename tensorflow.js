5e5c3ce90e75b39ad7bb83d42991b2827654bc96
Michel Promonet <michel.promonet@free.fr>
2020-04-24 22:14:51 +0200

    tries with posenet detection

--- tensorflow.js
+++ tensorflow.js
@@ -23,6 +23,41 @@ function runDetect(model, video, canvas) {
             predictions[i].bbox[1] > 10 ? predictions[i].bbox[1] - 5 : 10);
         }
 
-        window.setTimeout( ()=>{ this.runDetect(model, video, canvas); } , 0 );
+        window.setTimeout( ()=>{ runDetect(model, video, canvas); } , 0 );
+    });
+}
+
+function runPosenet(model, video, canvas) {
+
+    console.time('predict');
+    model.estimatePoses(video, {
+        decodingMethod: 'multi-person',
+        maxDetections: 5,
+        scoreThreshold: 0.1,
+        nmsRadius: 30
+      } ).then(poses => {
+        console.timeEnd('predict');
+      //  console.log('Predictions: ', poses);
+
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = '10px Arial';
+
+        poses.forEach(({score, keypoints}) => {
+                console.log('keypoints: ', keypoints);
+                for (let i = 0; i < keypoints.length; i++) {
+                    const keypoint = keypoints[i];
+                    if (keypoint.score >= 0.1) {
+                        const {y, x} = keypoint.position;
+                        console.log(keypoint.part, ' : ', x, "x", y);
+                        ctx.beginPath();
+                        ctx.arc(x, y, 3, 0, 2 * Math.PI);
+                        ctx.fillStyle = 'red';
+                        ctx.fill();
+                    }
+                }
+        });
+
+        window.setTimeout( ()=>{ runPosenet(model, video, canvas); } , 0 );
     });
 }
